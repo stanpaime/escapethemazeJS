e8248c222d207f471601fd73affe8d82aa8b427e
Stanpai <117434390+stanpaime@users.noreply.github.com>
2025-01-07 01:55:29 +0800

    Update script.js
    -added obstacle detection and edge detection, lose on touch
    -added win condition, win on touch
    -added score and highscore
    -revised speed to lower difficulty

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 var header = document.getElementById("startHeader");
+var myScore = document.getElementById("myTime");
+var time = 20;
+var highScore = document.getElementById("highScore");
 var id = null;
-var ex = 0;
-var ey = 0;
+var resetId = null;
+var ex = 5;
+var ey = 5;
 var elem = document.getElementById("gamePiece");
-var obs = document.getElementById("obs");
-var obs1 = document.getElementById("obs1");
-var obs2 = document.getElementById("obs2");
-var obstacles = [obs, obs1, obs2];
+var hobs1 = document.getElementById("hobs1");
+var hobs2 = document.getElementById("hobs2");
+var hobs3 = document.getElementById("hobs3");
+var vobs1 = document.getElementById("vobs1");
+var vobs2 = document.getElementById("vobs2");
+var vobs3 = document.getElementById("vobs3");
+var vobs4 = document.getElementById("vobs4");
+var vobs5 = document.getElementById("vobs5");
+var winTile = document.getElementById("winTile");
+var obstacles = [hobs1, hobs2, hobs3, vobs1, vobs2, vobs3, vobs4, vobs5];
+
 var keypress = null;
 
 function startGame() {
@@ -17,73 +28,129 @@ var gameArea = {
   start: function () {
     console.log("GAME START");
     clearInterval(id);
+    clearInterval(resetId);
     id = setInterval(frame, 20);
+    resetId = setInterval(reset, 200);
     window.addEventListener("keydown", function (e) {
       keypress = e.key;
-      console.log(keypress);
+      // console.log(keypress);
     });
-    console.log(obstacles);
     header.append("GAME START");
+    myTime.append("Time remaining: " + time + "s");
+    if (getScore() == 0) {
+      highScore.append("Latest remaining time: ");
+    } else {
+      highScore.append("Latest remaining time: " + getScore() + "s");
+    }
   },
   stop: function () {
     keypress = "";
     clearInterval(id);
     header.innerHTML = "GAME OVER";
+    reset();
   }
 };
 
 function reset() {
-  count = 0;
-  ex = 0;
-  ey = 0;
-  elem.style.top = ey + "px";
-  elem.style.left = ex + "px";
-  keypress = "";
-  mvmt = "";
-  id = setInterval(frame, 20);
+  if (keypress == "r") {
+    count = 0;
+    ex = 5;
+    ey = 5;
+    time = 20;
+    elem.style.top = ey + "px";
+    elem.style.left = ex + "px";
+    keypress = "";
+    mvmt = "";
+    clearInterval(id);
+    id = setInterval(frame, 20);
+    clearInterval(resetId);
+    resetId = setInterval(reset, 200);
+    header.innerHTML = "GAME START";
+    myTime.innerHTML = "Time remaining: " + time + "s";
+    highScore.innerHTML = "Latest remaining time: " + getScore() + "s";
+  }
 }
 
 function frame() {
+  // console.log(score);
   for (i = 0; i < obstacles.length; i++) {
     switch (keypress) {
       case "ArrowRight":
-        if (crash(obstacles[i]) || ex == 350) {
-          console.log("CRASHED!");
-          gameArea.stop();
+        if (crash(obstacles[i]) || ex >= 650) {
+          loseMessage();
         } else {
-          ex++;
+          // console.log(ex);
+          ex += 0.3;
           elem.style.left = ex + "px";
+          time -= 0.0025;
+          myTime.innerHTML = "Time remaining: " + Math.round(time) + "s";
         }
         break;
       case "ArrowLeft":
-        if (crash(obstacles[i]) || ex == 0) {
-          console.log("CRASHED!");
-          gameArea.stop();
+        if (crash(obstacles[i]) || ex <= 0) {
+          loseMessage();
         } else {
-          ex--;
+          ex -= 0.3;
           elem.style.left = ex + "px";
+          time -= 0.0025;
+          myTime.innerHTML = "Time remaining: " + Math.round(time) + "s";
         }
         break;
       case "ArrowDown":
-        if (crash(obstacles[i]) || ey == 350) {
-          console.log("CRASHED!");
-          gameArea.stop();
+        if (crash(obstacles[i]) || ey >= 450) {
+          loseMessage();
         } else {
-          ey++;
+          ey += 0.3;
           elem.style.top = ey + "px";
+          time -= 0.0025;
+          myTime.innerHTML = "Time remaining: " + Math.round(time) + "s";
         }
         break;
       case "ArrowUp":
-        if (crash(obstacles[i]) || ey == 0) {
-          console.log("CRASHED!");
-          gameArea.stop();
+        if (crash(obstacles[i]) || ey <= 0) {
+          loseMessage();
         } else {
-          ey--;
+          ey -= 0.3;
           elem.style.top = ey + "px";
+          time -= 0.0025;
+          myTime.innerHTML = "Time remaining: " + Math.round(time) + "s";
         }
         break;
     }
+
+    // console.log(ex);
+    // if (crash(obstacles[i]) || ex >= 650 || ex <= 0 || ey >= 450 || ey <= 0) {
+    //   loseMessage();
+    // }
   }
+
+  //Win the game
+  if (crash(winTile)) {
+    saveScore();
+    gameArea.stop();
+  } else if (time <= 0.5) {
+    loseMessage();
+  }
+}
+
+//Lose the game
+function loseMessage() {
+  console.log("CRASHED!");
+  highScore.innerHTML =
+    "You lost! Your latest remaining time is: " + Math.round(getScore()) + "s";
+  gameArea.stop();
+}
+
+function saveScore() {
+  sessionStorage.setItem("time", time);
+
+  highScore.innerHTML =
+    "You win! Your latest remaining time is: " + Math.round(getScore()) + "s";
+}
+
+function getScore() {
+  var latestScore = Math.round(sessionStorage.getItem("time"));
+  return latestScore;
 }
 
 function crash(otherobj) {
@@ -96,7 +163,7 @@ function crash(otherobj) {
   var otherright = otherobj.offsetLeft + otherobj.clientWidth;
   var othertop = otherobj.offsetTop;
   var otherbottom = otherobj.offsetTop + otherobj.clientHeight;
-  // console.log("Other left:" + otherleft);
+  // console.log("Other right:" + otherright);
 
   var crash = true;
   if (
